3dbdfdbeab40057e058044b94023dfc1e27d9ad2
Patrick Ens <patrick.ens@sp.senai.br>
2020-10-05 10:46:52 -0300

    init

---
+++ src/Nudes.Validator.Blazor/NudesValidator.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Nudes.Retornator.Core;
+using System;
+
+namespace Nudes.Validator.Blazor
+{
+    public class RetornatorValidator : ComponentBase
+    {
+        private ValidationMessageStore messageStore;
+
+        [CascadingParameter]
+        private EditContext CurrentEditContext { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (CurrentEditContext == null)
+            {
+                throw new InvalidOperationException(
+                    $"{nameof(RetornatorValidator)} requires a cascading " +
+                    $"parameter of type {nameof(EditContext)}. " +
+                    $"For example, you can use {nameof(RetornatorValidator)} " +
+                    $"inside an {nameof(EditForm)}.");
+            }
+
+            messageStore = new ValidationMessageStore(CurrentEditContext);
+
+            CurrentEditContext.OnValidationRequested += (s, e) =>
+                messageStore.Clear();
+            CurrentEditContext.OnFieldChanged += (s, e) =>
+                messageStore.Clear(e.FieldIdentifier);
+        }
+
+        public void DisplayError(Error error)
+        {
+            foreach (var err in error.FieldErrors)
+            {
+                messageStore.Add(CurrentEditContext.Field(err.Key), err.Value);
+            }
+
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+
+        public void ClearErrors()
+        {
+            messageStore.Clear();
+            CurrentEditContext.NotifyValidationStateChanged();
+        }
+    }
+}
---
+++ src/Nudes.Validator.MediatRBehavior/ValidationBehavior.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using MediatR;
+using Nudes.Retornator.Core;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nudes.Validator.MediatRBehavior
+{
+    public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TResponse : BaseResult<TResponse>, new()
+    {
+        private readonly IEnumerable<IValidator<TRequest>> validators;
+
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            this.validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var failedValidations = validators
+                .Select(v => v.Validate(request))
+                .Where(v => !v.IsValid)
+                .ToList();
+
+            if (failedValidations.Any())
+            {
+                var errors = failedValidations
+                    .SelectMany(f => f.Errors)
+                    .GroupBy(d => d.PropertyName)
+                    .ToDictionary(d => d.Key, d => d.Select(f => f.ErrorMessage)
+                    .ToList());
+
+                return BaseResult<TResponse>.Throw(new Error()
+                {
+                    FieldErrors = errors
+                });
+            }
+
+            return await next();
+        }
+    }
+}
